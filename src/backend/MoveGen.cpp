5a23ba469360842d473bce10fe2a19086f8343f4
taperihn0 <szymonstar690@gmail.com>
2026-06-30 20:13:25 +0200

    Little change

--- src/backend/MoveGen.cpp
+++ src/backend/MoveGen.cpp
@@ -566,7 +566,9 @@ void generateMovesInMode(const Position& pos, MoveList& move_list) {
     const enumColor side2move = pos.getTurn();
     const BitBoard  enemy_pieces = pos.getOppositePieces(),
                     occupied = pos.getOccupied(),
-                    checkers = pos.getCheckers(side2move);
+                    checkers = LMode == PSEUDOLEGAL ? pos.getWeakestCheckers(side2move) 
+                                                    : pos.getCheckers(side2move);
+                                                    
     const CacheKingRelated cache = getCache<LMode>(pos, side2move);
 
     if (side2move == WHITE) {
