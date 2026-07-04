0603b9a3ad591418ccdcc1367288e6f19902c197
taperihn0 <szymonstar690@gmail.com>
2026-07-04 22:57:28 +0200

    Change checkers mask in move gen

--- src/backend/MoveGen.cpp
+++ src/backend/MoveGen.cpp
@@ -579,8 +579,7 @@ void generateMovesInMode(const Position& pos, MoveList& move_list) {
     const enumColor side2move = pos.getTurn();
     const BitBoard  enemy_pieces = pos.getOppositePieces(),
                     occupied = pos.getOccupied(),
-                    checkers = LMode == PSEUDOLEGAL ? pos.getWeakestCheckers(side2move) 
-                                                    : pos.getCheckers(side2move);
+                    checkers = pos.getCheckers(side2move);
 
     const CacheKingRelated cache = getCache<LMode>(pos, side2move);
 
