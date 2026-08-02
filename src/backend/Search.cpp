503194894fcae3236ee8ed3ef98560171b994795
taperihn0 <szymonstar690@gmail.com>
2026-08-02 15:07:44 +0200

    .

--- src/backend/Search.cpp
+++ src/backend/Search.cpp
@@ -1095,7 +1095,7 @@ Score Search::nmSearch(Position& pos,
 
                 const int32_t unorm_score = static_cast<int32_t>(node->move_picker.getPositiveNormQuietScore(node->move, 
                                                                                                              node->side2move));
-                const int32_t futility_margin = FutilityDelta * depth * depth + unorm_score * 18 / 8192;
+                const int32_t futility_margin = FutilityDelta * depth * depth + unorm_score * 26 / 8192;
 
                 if (node->eval + futility_margin < alpha) {
                     node->score = alpha;
