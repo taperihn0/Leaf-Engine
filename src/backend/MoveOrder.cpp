6874140910d64c588f83c056d6760fb831210375
taperihn0 <szymonstar690@gmail.com>
2026-08-17 16:22:41 +0200

    Static quiet score

--- src/backend/MoveOrder.cpp
+++ src/backend/MoveOrder.cpp
@@ -115,12 +115,12 @@ bool MoveOrder::nextMoveWithPolicy(const search::NodeInfo* node,
     case enumPrivateStage::STAGED_QUIETS:
         assert(Policy != QUIESCENT);
         MoveGen::generatePseudoLegalMoves<MoveGen::QUIETS>(pos, _move_list);
+        scoreQuiets(_quiets_ind, pos.getTurn());
 
         _stage = enumPrivateStage::STAGED_PICK_QUIETS;
         [[fallthrough]];
     case enumPrivateStage::STAGED_PICK_QUIETS:
         assert(Policy != QUIESCENT);
-        scoreQuiets(_iterator, pos.getTurn());
         return nextFromList(next_move, move_score);
     default:
         assert(false);
