f38ce33bb321cfc32dccb5c42ccb3ee1ff858de7
taperihn0 <szymonstar690@gmail.com>
2026-09-05 13:21:32 +0200

    .

--- src/backend/Search.cpp
+++ src/backend/Search.cpp
@@ -1582,7 +1582,7 @@ _NODISCARD _FORCEINLINE int32_t Search::getScoreMoveReduction<QUIET>(mvo::SMoveS
 template <>
 _NODISCARD _FORCEINLINE int32_t Search::getScoreMoveReduction<CAPTURE>(mvo::SMoveScore s) {
     // TODO: better fixed-point formula
-    return (CaptureMoveScoreReductionRate * (50 - s.value())) / 128;
+    return 5 - (CaptureMoveScoreReductionRate * s.value()) / 128;
 }
 
 // =================================
